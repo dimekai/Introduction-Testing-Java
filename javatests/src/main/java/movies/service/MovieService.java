63557d43ac1bf247acf30ff5f34d415132f0e92e
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-05-16 14:52:03 -0500

    Se ha agregado la funcion findMovieByTemplate

--- javatests/src/main/java/movies/service/MovieService.java
+++ javatests/src/main/java/movies/service/MovieService.java
@@ -22,19 +22,58 @@ public class MovieService {
                 .filter(movie -> movie.getGenre() == genre).collect(Collectors.toList());
     }
 
-    public Collection<Movie> findMoviesByTime(int minutes) {
+    public Collection<Movie> findMoviesByTime(Integer minutes) {
         return this.movieRepository.findAll().stream()
                 .filter(movie -> movie.getMinutes() <= minutes).collect(Collectors.toList());
     }
-    
+
     public Collection<Movie> findMovieByName(String name) {
         return this.movieRepository.findAll().stream()
                 .filter(movie -> movie.getName().toLowerCase().contains(name.toLowerCase())).collect(Collectors.toList());
     }
-    
+
     public Collection<Movie> findMovieByDirector(String director) {
         return this.movieRepository.findAll().stream()
                 .filter(movie -> movie.getDirector().toLowerCase().contains(director.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public Collection<Movie> templateFilter(Movie template, Collection<Movie> movies) {
+
+        if (template.getGenre() != null) {
+            movies = movies.stream()
+                    .filter(movie -> movie.getGenre() == template.getGenre())
+                    .collect(Collectors.toList());
+        }
+
+        if (template.getMinutes() != null && template.getMinutes() >= 0) {
+            movies = movies.stream()
+                    .filter(movie -> movie.getMinutes() <= template.getMinutes())
+                    .collect(Collectors.toList());
+        }
+
+        if (template.getName() != null) {
+            movies = movies.stream()
+                    .filter(movie -> movie.getName().toLowerCase().contains(template.getName().toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+
+        if (template.getDirector() != null) {
+            movies = movies.stream()
+                    .filter(movie -> movie.getDirector().toLowerCase().contains(template.getDirector().toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+
+        return movies;
+
+    }
+
+    public Collection<Movie> findMovieByTemplate(Movie template) {
+
+        Collection<Movie> movies = movieRepository.findAll();
+
+        return (template.getId() == null)
+                ? templateFilter(template, movies)
+                : movies.stream().filter(movie -> movie.getId().equals(template.getId())).collect(Collectors.toList());
     }
 
 }
